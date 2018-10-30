3890a4e85b333e74be35c4e513af8d31d68e1d68
luckboystudent <luckboystudent@gmail.com>
2018-10-30 21:28:01 +0800

    20181030

--- HW1/question1/Q2.js
+++ HW1/question1/Q2.js
@@ -3,4 +3,4 @@ const keccak256= require('js-sha3').keccak256;
 const wallet = Wallet.generate();
 
 let public_key_hash=keccak256(wallet.getPublicKey());
-console.log(`0x${public_key_hash.substring(public_key_hash.length-20,public_key_hash.length)}`);
+console.log(`0x${public_key_hash.substring(public_key_hash.length-40,public_key_hash.length)}`);
